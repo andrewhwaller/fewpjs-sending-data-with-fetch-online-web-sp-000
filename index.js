d2f161bef51fa0058cf16018575e652b9daf3a82
Andrew Waller <andrewhwaller@gmail.com>
2019-10-01 02:41:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function submitData(name, email) {
   })
   .catch(function(error) {
     let errorHeading = document.querySelector('h1')
-  errorHeading.appendChild(error)
+    errorHeading.appendChild(error)
   })
 }
