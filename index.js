ffe30263ec8a10363fce0ac8964f56f17f1825d1
Andrew Waller <andrewhwaller@gmail.com>
2019-10-01 02:35:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Add your code here
 function submitData(name, email) {
-
+  let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
 }
